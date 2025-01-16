8383a0aff78a452d0e6575b149cb603799550e60
yamashita yuichi <crgone910428@gmail.com>
2025-01-17 00:44:28 +0900

    エラー表示用のカスタムフックを追加

---
+++ src/lib/config/constants/index.ts
@@ -0,0 +1,20 @@
+export const API_URL = import.meta.env.VITE_API_URL
+
+export const ERROR_TYPES = {
+  INVALID_CSRF_TOKEN: 'invalid csrf token',
+  INVALID_JWT: 'invalid or expired jwt',
+  MALFORMED_JWT: 'missing or malformed jwt',
+  DUPLICATE_KEY: 'duplicated key not allowed',
+  INVALID_PASSWORD:
+    'crypto/bcrypt: hashedPassword is not the hash of the given password',
+  RECORD_NOT_FOUND: 'record not found',
+} as const
+
+export const ERROR_MESSAGES = {
+  [ERROR_TYPES.INVALID_CSRF_TOKEN]: 'CSRF token is invalid, please try again',
+  [ERROR_TYPES.INVALID_JWT]: 'access token expired, please login',
+  [ERROR_TYPES.MALFORMED_JWT]: 'access token is not valid, please login',
+  [ERROR_TYPES.DUPLICATE_KEY]: 'email already exist, please use another one',
+  [ERROR_TYPES.INVALID_PASSWORD]: 'password is not correct',
+  [ERROR_TYPES.RECORD_NOT_FOUND]: 'email is not correct',
+} as const
---
+++ src/lib/hooks/useError.ts
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router-dom'
+import useStore from '../../store'
+import { ERROR_MESSAGES, ERROR_TYPES } from '../config/constants'
+import { getCsrfToken } from '../api/client'
+
+export const useError = () => {
+  const navigate = useNavigate()
+  const resetEditedTask = useStore((state) => state.resetEditedTask)
+
+  const switchErrorHandling = (msg: string) => {
+    switch (msg) {
+      case ERROR_TYPES.INVALID_CSRF_TOKEN:
+        getCsrfToken()
+        alert(ERROR_MESSAGES[ERROR_TYPES.INVALID_CSRF_TOKEN])
+        break
+      case ERROR_TYPES.INVALID_JWT:
+        alert(ERROR_MESSAGES[ERROR_TYPES.INVALID_JWT])
+        resetEditedTask()
+        navigate('/')
+        break
+      case ERROR_TYPES.DUPLICATE_KEY:
+        alert(ERROR_MESSAGES[ERROR_TYPES.DUPLICATE_KEY])
+        break
+      case ERROR_TYPES.INVALID_PASSWORD:
+        alert(ERROR_MESSAGES[ERROR_TYPES.INVALID_PASSWORD])
+        break
+      case ERROR_TYPES.RECORD_NOT_FOUND:
+        alert(ERROR_MESSAGES[ERROR_TYPES.RECORD_NOT_FOUND])
+        break
+      default:
+        alert(msg)
+    }
+  }
+  return { switchErrorHandling }
+}
