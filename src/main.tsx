028c3595a519ccd6e84fb665d835b26c4ec9597a
yamashita yuichi <crgone.910428.en@gmail.com>
2025-01-27 00:45:00 +0900

    リファクタ

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { Router } from './Router'
+import { Router } from './router'
 
 const queryClient = new QueryClient({})
 
